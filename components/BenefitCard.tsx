d952df0e2ae84a683b63ce61d1aea4787f8363b6
Matthew Poll <mrp@Matthews-Mac-mini.local>
2025-11-24 14:41:16 +1000

    Fix: Reduce benefit icon size by ~16% (96px to 80px)

--- components/BenefitCard.tsx
+++ components/BenefitCard.tsx
@@ -9,12 +9,12 @@ interface BenefitCardProps {
 export default function BenefitCard({ title, description, icon }: BenefitCardProps) {
   return (
     <div className="flex flex-col items-center text-center p-6 rounded-lg hover:bg-white/10 transition-colors">
-      <div className="mb-6 w-24 h-24 relative">
+      <div className="mb-6 w-20 h-20 relative">
         <Image 
           src={icon} 
           alt={title}
-          width={96}
-          height={96}
+          width={80}
+          height={80}
           className="object-contain brightness-0 invert"
         />
       </div>
